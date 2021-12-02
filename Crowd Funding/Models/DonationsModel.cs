a0b5efca247f5873702c7244d4295a0681ab3d2e
moshersbr <95356427+moshersbr@users.noreply.github.com>
2021-12-02 15:13:03 +0100

    New Model DonationsModel
    DonationsModel.cs added

---
+++ Crowd Funding/Models/DonationsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Crowd_Funding.Models
+{
+    public class DonationsModel
+    {
+        public double amount { get; set; }
+        public string date { get; set; }
+        public string goal { get; set; }
+        public string reward { get; set; }
+        public int customerid { get; set; }
+    }
+}
