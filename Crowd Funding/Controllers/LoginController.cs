04a62c956a1c0db641ad42b14319c07cc71947bd
Saad, Abdelhadi - Student Rijn IJssel <00293388@rijnijssel.nl>
2021-12-02 14:07:50 +0100

    commit

---
+++ Crowd Funding/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Crowd_Funding.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crowd_Funding.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult login(UsersModel usersModel)
+        {
+            if (usersModel.email == "" && usersModel.password == "")
+            {
+                return View("Home", usersModel);
+
+            } else
+            {
+                return View("index"); 
+            }
+        }
+    }
+}
