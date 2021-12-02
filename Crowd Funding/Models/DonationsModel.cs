737ea45ffad293501bdb802ab109e1a43099f8f9
moshersbr <95356427+moshersbr@users.noreply.github.com>
2021-12-02 15:29:29 +0100

    Variable Names
    Changed variables names inside DonationsModel.cs to match database variable names.

--- Crowd Funding/Models/DonationsModel.cs
+++ Crowd Funding/Models/DonationsModel.cs
@@ -5,8 +5,8 @@ namespace Crowd_Funding.Models
     {
         public double amount { get; set; }
         public string date { get; set; }
-        public string goal { get; set; }
-        public string reward { get; set; }
-        public int customerid { get; set; }
+        public int goal_id { get; set; }
+        public int reward_id { get; set; }
+        public int user_id { get; set; }
     }
 }
